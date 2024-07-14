48e5c87512c5a9493e1794ae1c21a339e2bf2525
Aben <126703861+abengl@users.noreply.github.com>
2024-07-13 22:05:41 -0500

    build: output view on the DOM

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 //<textarea name="output" id="" class="main__result__textarea"></textarea>
 //<button class="main__result__button" id="button__copy">Copiar</button>
+
+/*Activate output area when there is text in the input */
+const inputElement = document.getElementById("input");
+const encryptButton = document.getElementById("button__encrypt");
+const dencryptButton = document.getElementById("button__dencrypt");
+
+const outputContainer = document.querySelector(".main__result");
+const outputElement = document.createElement("textarea");
+outputElement.id = "output";
+
+const copyButton = document.createElement("button");
+copyButton.innerText =  "Copiar";
+copyButton.id = "button__copy";
+
+document.addEventListener("DOMContentLoaded", () => {
+    encryptButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        const inputText = inputElement.value;
+
+        outputContainer.innerHTML = "";
+        outputContainer.append(outputElement, copyButton);
+        outputElement.value = inputText;
+    })
+})
