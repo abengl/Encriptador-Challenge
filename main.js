a66d995a55c07776b56147765e19a87a5bb87961
Aben <126703861+abengl@users.noreply.github.com>
2024-07-14 18:26:07 -0500

    feature: encrypt function implemented #4

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-//<textarea name="output" id="" class="main__result__textarea"></textarea>
-//<button class="main__result__button" id="button__copy">Copiar</button>
-
-/*Activate output area when there is text in the input */
 const inputElement = document.getElementById("input");
 const encryptButton = document.getElementById("button__encrypt");
 const dencryptButton = document.getElementById("button__dencrypt");
@@ -13,16 +9,52 @@ outputElement.setAttribute("name", "output");
 outputElement.setAttribute("rows", "8");
 
 const copyButton = document.createElement("button");
-copyButton.innerText =  "Copiar";
+copyButton.innerText = "Copiar";
 copyButton.id = "button__copy";
 
-document.addEventListener("DOMContentLoaded", () => {
-    encryptButton.addEventListener("click", (event) => {
-        event.preventDefault();
-        const inputText = inputElement.value;
+/**
+ * Checks for lowercase non-special characters
+ * @param {input}
+ */
+function isValid(input) {
+  const pattern = /^(?=[a-z])[a-z\s]+$/g;
+  if (!pattern.test(input)) {
+    alert("Por favor, usa minúsculas sin carácteres especiales o acentos.");
+    return false;
+  }
+  return true;
+}
 
-        outputContainer.innerHTML = "";
-        outputContainer.append(outputElement, copyButton);
-        outputElement.value = inputText;
+/**
+ *
+ * @param {text} input from textarea, it must contain lowercase without special characters
+ */
+function encrypt(text) {
+  const arrayInput = text.split("");
+  const encrypted = arrayInput
+    .map((element) => {
+      if (element === "e") return "enter";
+      else if (element === "i") return "imes";
+      else if (element === "a") return "ai";
+      else if (element === "o") return "ober";
+      else if (element === "u") return "ufat";
+      else return element;
     })
-})
+    .join("");
+  return [encrypted, text];
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  encryptButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const inputText = inputElement.value;
+
+    if(!isValid(inputText)) {
+        return;
+    }
+
+    outputContainer.innerHTML = "";
+    outputContainer.append(outputElement, copyButton);
+    outputElement.value = encrypt(inputText)[0];
+  });
+});
